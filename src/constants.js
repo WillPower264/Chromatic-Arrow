56f9d9dc06c71290b8067231470634b096e938e0
WilliamLi264264 <william.li@princeton.edu>
2020-05-11 19:38:27 -0400

    damnit i did it again

--- src/constants.js
+++ src/constants.js
@@ -124,7 +124,7 @@ const CONSTS = {
           maxSpeed: 4,
         },
         numBarriers: 15,
-        timeLimit: 6,          // in seconds
+        timeLimit: 60,          // in seconds
         msEndDelay: 1 * 1000,
     },
     scoreBox: {
